8c96b736865d992b70c8ec9ee66037a3c5217734
Zakhar Kravchuk <zakr600@yandex.ru>
2026-05-02 16:05:31 +0300

    backups to s3

--- docker/postgres-backup.sh
+++ docker/postgres-backup.sh
@@ -3,15 +3,25 @@ set -eu
 
 BACKUP_DIR="${POSTGRES_BACKUP_DIR:-/backups}"
 BACKUP_PREFIX="${POSTGRES_BACKUP_PREFIX:-postgres}"
-BACKUP_KEEP_DAYS="${POSTGRES_BACKUP_KEEP_DAYS:-7}"
 BACKUP_INTERVAL_SECONDS="${POSTGRES_BACKUP_INTERVAL_SECONDS:-86400}"
 POSTGRES_HOST="${POSTGRES_HOST:-postgres}"
 POSTGRES_PORT="${POSTGRES_PORT:-5432}"
 POSTGRES_DB="${POSTGRES_DB:-sostra}"
 POSTGRES_USER="${POSTGRES_USER:-sostra}"
+S3_BACKUP_BUCKET="${S3_BACKUP_BUCKET:-}"
+S3_BACKUP_PREFIX="${S3_BACKUP_PREFIX:-postgres}"
+S3_BACKUP_ENDPOINT_URL="${S3_BACKUP_ENDPOINT_URL:-}"
 export PGPASSWORD="${POSTGRES_PASSWORD:-}"
 
 mkdir -p "${BACKUP_DIR}"
+
+aws_s3() {
+  if [ -n "${S3_BACKUP_ENDPOINT_URL}" ]; then
+    aws --endpoint-url "${S3_BACKUP_ENDPOINT_URL}" "$@"
+  else
+    aws "$@"
+  fi
+}
 
 wait_for_postgres() {
   until pg_isready -h "${POSTGRES_HOST}" -p "${POSTGRES_PORT}" -U "${POSTGRES_USER}" -d "${POSTGRES_DB}" >/dev/null 2>&1; do
@@ -34,8 +44,11 @@ create_backup() {
     return 1
   fi
 
-  echo "Removing backups older than ${BACKUP_KEEP_DAYS} days from ${BACKUP_DIR}"
-  find "${BACKUP_DIR}" -type f -name "${BACKUP_PREFIX}_${POSTGRES_DB}_*.sql.gz" -mtime "+$((BACKUP_KEEP_DAYS - 1))" -delete
+  if [ -n "${S3_BACKUP_BUCKET}" ]; then
+    s3_key="${S3_BACKUP_PREFIX}/${BACKUP_PREFIX}_${POSTGRES_DB}_${timestamp}.sql.gz"
+    echo "Uploading backup to s3://${S3_BACKUP_BUCKET}/${s3_key}"
+    aws_s3 s3 cp "${backup_file}" "s3://${S3_BACKUP_BUCKET}/${s3_key}"
+  fi
 }
 
 wait_for_postgres
